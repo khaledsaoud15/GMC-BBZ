4f1127ae7493f01505ca2b6f94a64a3c3fc4e832
khaled saoud <khaledsaoud237@gmail.com>
2022-12-01 13:12:36 +0100

    new

---
+++ chapter3/condition.js
@@ -0,0 +1,13 @@
+// const user = parseInt(prompt("enter a number"));
+
+// if (typeof user === "number" && user % 2 === 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+/* 
+Print num1 is the largest if num1>num2.
+Print num2 is the largest if num1<num2.
+Else print num1 and num2 are equal when num1==num2.
+*/
