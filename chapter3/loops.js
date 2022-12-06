8ba55014c6c73310cb6c0446807f73c2f826c56d
khaled saoud <khaledsaoud237@gmail.com>
2022-12-06 12:50:16 +0100

    commit

---
+++ chapter3/loops.js
@@ -0,0 +1,27 @@
+let i = 0;
+
+const letter = "khaled";
+
+// for (i; i <= 8; i++) {
+//   if (i == 2) {
+//     console.log(i);
+//     break;
+//   }
+// }
+
+// console.log("----------------------------------------------------");
+
+// let k = 0;
+// while (k <= 5) {
+//   console.log(k);
+//   k++;
+// }
+
+// console.log("----------------------------------------------------");
+
+// let j = 0;
+
+// do {
+//   console.log(j);
+//   j++;
+// } while (j >= 5);
